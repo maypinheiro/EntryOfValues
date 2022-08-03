87897b3cbb8bcb8e141b937b7b1bd030d5b672d4
maypinheiro <mayarapinheirodesousa@hotmail.com>
2022-08-03 17:07:27 -0300

    Exercicio

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace entryOfValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = (int.Parse(Console.ReadLine()));
+            int B = (int.Parse(Console.ReadLine()));
+
+            int X = A + B;
+
+            Console.WriteLine($"X = {X}");
+        }
+    }
+}
